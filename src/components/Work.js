320778b30781f64f62b2e69b0772c53b7edc808a
toretto yvan <yvan.munyangabe@gmail.com>
2023-06-27 19:52:08 +0300

    service page is done

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,35 +2,103 @@ import React from "react";
 //motion
 import { motion } from "framer-motion";
 //variants
-import { fadIn } from "../variants";
+import { fadeIn } from "../variants";
 // image
 import Img1 from "../assets/portfolio-img1.png";
 import Img2 from "../assets/portfolio-img2.png";
 import Img3 from "../assets/portfolio-img3.png";
+import { Button } from "react-scroll";
 
 const Work = () => {
   return (
     <section className="section" id="Work">
       <div className="container mx-auto">
-        <div>
-          {/* ttext  */}
-          <div>
-            <h2 className="h2 leading-tight text-accent">
-              my Latest <br />
-              Work.
-            </h2>
-            <p className="max-w-sm mb-16">
-              Amet laborum adipisicing eu aliqua duis officia quis amet velit
-              fugiat nostrud anim. Quis duis voluptate aliqua pariatur nisi
-              consectetur ex eiusmod et tempor. Sint incididunt aliquip irure
-              dolore ad labore cupidatat aliquip excepteur id in qui. Est labore
-              laboris minim deserunt cupidatat fugiat consequat minim esse
-              laborum in eu aliqua. Veniam laborum id laborum consequat ea
-              tempor nisi incididunt ex.
-            </p>
-          </div>
-          {/* Image  */}
-          <div>2</div>
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ oncuechange: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+            {/* text  */}
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                my Latest <br />
+                Work.
+              </h2>
+              <p className="max-w-sm mb-16">
+                Amet laborum adipisicing eu aliqua duis officia quis amet velit
+                fugiat nostrud anim. Quis duis voluptate aliqua pariatur nisi
+                conses
+              </p>
+              <button className="btn btn-sm">View all projects</button>
+            </div>
+            {/* Image  */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-x1">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design </span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ oncuechange: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-10">
+            {/* Image 2 */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-x1">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img2}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design </span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+
+            {/* Image 3 */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-x1">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img3}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design </span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
