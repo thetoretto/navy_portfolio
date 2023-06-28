a8194263fa53f3c4c04623d9b071d340a24188ec
toretto yvan <yvan.munyangabe@gmail.com>
2023-06-28 23:00:50 +0300

    web page is done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,63 @@
 import React from "react";
+//motion
+import { motion } from "framer-motion";
+
+//variants
+import { fadeIn } from "../variants";
 
 const Contact = () => {
   return (
-    <div className="section" id="Contact">
-      Contact
-    </div>
+    <section className="py-16 lg:section" id="Contact">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ oncuechange: false, amount: 0.3 }}
+            className="flex-1">
+            <div>
+              <h4
+                className="text-x1 uppercase text-accent font-medium mb-2
+               tracking-wide">
+                Get in touch
+              </h4>
+              <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">
+                Let's work <br /> together !
+              </h2>
+            </div>
+          </motion.div>
+          {/* form  */}
+          <motion.form
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ oncuechange: false, amount: 0.3 }}
+            className="flex-1 border rounded-2x1 flex flex-col gap-y-6 
+          pb-24 p-6 items-start">
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full
+            placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your email"
+            />
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full
+            placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your Names"
+            />
+            <textarea
+              className="bg-transparent border-b py-12 outline-none w-full
+            placeholder:text-white focus:border-accent transition-all 
+            resize-none mb-12 "
+              placeholder="Your message"></textarea>
+            <button className="btn btn-lg">send a message</button>
+          </motion.form>
+        </div>
+      </div>
+    </section>
   );
 };
 
